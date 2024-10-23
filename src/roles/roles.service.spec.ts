9ee64fa7bda35711a5ccdfbed1055fd7c0ed5b4a
Ernor1 <honorerukundo74@gmail.com>
2024-10-23 06:23:16 +0200

    [TEST] pass_init

--- src/roles/roles.service.spec.ts
+++ src/roles/roles.service.spec.ts
@@ -1,13 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RolesService } from './roles.service';
 import { DatabaseService } from 'src/database/database.service';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
 
 describe('RolesService', () => {
   let service: RolesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RolesService, DatabaseService],
+      providers: [RolesService, DatabaseService, JwtService, UsersService],
     }).compile();
 
     service = module.get<RolesService>(RolesService);
